e44c2b3ddedf7e10e83a81f5ef44bc005fdb48c0
PushyTao <169961689@qq.com>
2022-06-07 22:03:16 +0800

    add result entity

--- src/main/java/cn/edu/ldu/watercloud/controller/UseWaterController.java
+++ src/main/java/cn/edu/ldu/watercloud/controller/UseWaterController.java
@@ -1,7 +1,9 @@
 package cn.edu.ldu.watercloud.controller;
 
+import cn.edu.ldu.watercloud.entity.result.WaterFormYear;
 import cn.edu.ldu.watercloud.mapper.UseWaterMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,20 +18,19 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/yiqing_war/water")
+@CrossOrigin(origins = "http://127.0.0.1:5500", allowedHeaders = "*", allowCredentials = "true")
 public class UseWaterController {
     @Autowired
     UseWaterMapper useWaterMapper;
     @PostMapping("/selectWaterFormYear")
-    public List SelectWaterFormYear() {
-        List ret = new ArrayList<>();
-        ret.add("wuyt");
-        ret.add("1");
-        ret.add("2022");
-        ret.add("1");
-        ret.add("2");
-        ret.add("3");
-        ret.add(String.valueOf(useWaterMapper.queryByQuarter()));
-//        useWaterMapper.queryByQuarter();
+    public List<WaterFormYear> SelectWaterFormYear() {
+        List ret = new ArrayList();
+        WaterFormYear  waterFormYear = new WaterFormYear("wuyt","1","2022","1",
+                "2","3",String.valueOf(useWaterMapper.queryByQuarter()));
+        ret.add(waterFormYear);
+        ret.add(waterFormYear);
+        ret.add(waterFormYear);
+        System.out.println("request +1");
         return ret;
     }
 }
