a45aaa020134dd29d1dff5fda1ede95c3d68f02c
95705 <plonk_r4z5@163.com>
2022-06-07 16:41:27 +0800

    修改Buy类为lombok格式

--- src/main/java/cn/edu/ldu/watercloud/entity/Buy.java
+++ src/main/java/cn/edu/ldu/watercloud/entity/Buy.java
@@ -1,10 +1,18 @@
 package cn.edu.ldu.watercloud.entity;
 
+import lombok.*;
+
 /**
  * @author wuyt
  * @data 2022/6/7
  * @apiNote
  */
+@Data//lombok
+@NoArgsConstructor//无参构造
+@Getter
+@Setter
+@AllArgsConstructor//全参构造
+@ToString
 public class Buy {
     private int id;
     private double amount;
@@ -13,83 +21,4 @@ public class Buy {
     private int year;
     private int month;
     private int day;
-
-    public Buy(int id, double amount, double price, String provider, int year, int month, int day) {
-        this.id = id;
-        this.amount = amount;
-        this.price = price;
-        this.provider = provider;
-        this.year = year;
-        this.month = month;
-        this.day = day;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getProvider() {
-        return provider;
-    }
-
-    public void setProvider(String provider) {
-        this.provider = provider;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    @Override
-    public String toString() {
-        return "Buy{" +
-                "id=" + id +
-                ", amount=" + amount +
-                ", price=" + price +
-                ", provider='" + provider + '\'' +
-                ", year=" + year +
-                ", month=" + month +
-                ", day=" + day +
-                '}';
-    }
 }
