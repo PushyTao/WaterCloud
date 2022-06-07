5090786b8332b6822c6c5e37f25a1be3cc117be6
95705 <plonk_r4z5@163.com>
2022-06-07 22:13:47 +0800

    实现收费日报与月报sql。

---
+++ src/main/java/cn/edu/ldu/watercloud/entity/Monthly_charge.java
@@ -0,0 +1,17 @@
+package cn.edu.ldu.watercloud.entity;
+
+import lombok.*;
+
+@Data//lombok
+@NoArgsConstructor//无参构造
+@Getter
+@Setter
+@AllArgsConstructor//全参构造
+@ToString
+public class Monthly_charge {
+    private String project_name;//项目名
+    private int house_num;//户数
+    private double water_price;//单价
+    private double amount_sum;//数量
+    private double cost_sum;//金额
+}
--- src/main/java/cn/edu/ldu/watercloud/mapper/ProjectMapper.java
+++ src/main/java/cn/edu/ldu/watercloud/mapper/ProjectMapper.java
@@ -2,11 +2,22 @@ package cn.edu.ldu.watercloud.mapper;
 
 
 import cn.edu.ldu.watercloud.entity.Charge_day;
+import cn.edu.ldu.watercloud.entity.Daily_charge;
+import cn.edu.ldu.watercloud.entity.Monthly_charge;
 import cn.edu.ldu.watercloud.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ProjectMapper {
 
     Charge_day selectByUserAndUseWaterAndProject();
+
+    @Select("SELECT project_name,house_num,water_price,amount_sum,cost_sum from (select project.projname as project_name,COUNT(user.name) as house_num,project.price as water_price,SUM(usewater.amount) as amount_sum,SUM(usewater.amount)*project.price as cost_sum from project,user,usewater where user.id= usewater.userid and usewater.year=#{year} and usewater.month=#{month} and usewater.day=#{day}) as Daily_charge")
+    List<Daily_charge> daily_charge(int year, int month, int day);
+    @Select("SELECT project_name,house_num,water_price,amount_sum,cost_sum from (select project.projname as project_name,COUNT(user.name) as house_num,project.price as water_price,SUM(usewater.amount) as amount_sum,SUM(usewater.amount)*project.price as cost_sum from project,user,usewater where user.id= usewater.userid and usewater.year=#{year} and usewater.month=#{month} ) as Monthly_charge")
+    List<Monthly_charge> monthly_charge(int year, int month);
+
 }
