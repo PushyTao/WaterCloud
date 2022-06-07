ff49bb2bcd0596c330b220e48e95270a4ceb7c0b
PushyTao <169961689@qq.com>
2022-06-07 10:10:17 +0800

    init project

---
+++ src/test/java/cn/edu/ldu/watercloud/WaterCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.ldu.watercloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
