0949120fa8a929cd681858159f32832321802bea
95705 <plonk_r4z5@163.com>
2022-06-07 22:20:03 +0800

    将Project中的Charge_day修改为ChargeDay。 添加测试方法test_Daily_charge_and_Monthly_charge

--- src/main/java/cn/edu/ldu/watercloud/mapper/ProjectMapper.java
+++ src/main/java/cn/edu/ldu/watercloud/mapper/ProjectMapper.java
@@ -1,7 +1,6 @@
 package cn.edu.ldu.watercloud.mapper;
 
-
-import cn.edu.ldu.watercloud.entity.Charge_day;
+import cn.edu.ldu.watercloud.entity.ChargeDay;
 import cn.edu.ldu.watercloud.entity.Daily_charge;
 import cn.edu.ldu.watercloud.entity.Monthly_charge;
 import cn.edu.ldu.watercloud.entity.User;
@@ -13,7 +12,7 @@ import java.util.List;
 @Mapper
 public interface ProjectMapper {
 
-    Charge_day selectByUserAndUseWaterAndProject();
+    ChargeDay selectByUserAndUseWaterAndProject();
 
     @Select("SELECT project_name,house_num,water_price,amount_sum,cost_sum from (select project.projname as project_name,COUNT(user.name) as house_num,project.price as water_price,SUM(usewater.amount) as amount_sum,SUM(usewater.amount)*project.price as cost_sum from project,user,usewater where user.id= usewater.userid and usewater.year=#{year} and usewater.month=#{month} and usewater.day=#{day}) as Daily_charge")
     List<Daily_charge> daily_charge(int year, int month, int day);
--- src/test/java/cn/edu/ldu/watercloud/WaterCloudApplicationTests.java
+++ src/test/java/cn/edu/ldu/watercloud/WaterCloudApplicationTests.java
@@ -1,5 +1,7 @@
 package cn.edu.ldu.watercloud;
 
+import cn.edu.ldu.watercloud.entity.Daily_charge;
+import cn.edu.ldu.watercloud.entity.Monthly_charge;
 import cn.edu.ldu.watercloud.mapper.ProjectMapper;
 import cn.edu.ldu.watercloud.mapper.UseWaterMapper;
 import org.junit.jupiter.api.Test;
@@ -41,4 +43,21 @@ class WaterCloudApplicationTests {
 		System.out.println(ret.toString());
 	}
 
+	@Test
+	public void test_Daily_charge_and_Monthly_charge(){
+		String str = "2022-06-07";
+		//现将年月日切分出来
+		String[] arr = str.split("-");
+		int year = Integer.parseInt(arr[0]);
+		int month = Integer.parseInt(arr[1]);
+		int day = Integer.parseInt(arr[2]);
+		List<Daily_charge> list = projectMapper.daily_charge(year,month,day);
+		for (Daily_charge i: list){
+			System.out.println(i.toString());
+		}
+		List<Monthly_charge> list2 = projectMapper.monthly_charge(year,month);
+		for (Monthly_charge i: list2){
+			System.out.println(i.toString());
+		}
+	}
 }
