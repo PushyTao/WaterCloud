825ed7382efa38986e448d50d5fb4678eb12e970
95705 <plonk_r4z5@163.com>
2022-06-08 09:40:31 +0800

    修改sql语句，并测试

--- src/test/java/cn/edu/ldu/watercloud/WaterCloudApplicationTests.java
+++ src/test/java/cn/edu/ldu/watercloud/WaterCloudApplicationTests.java
@@ -44,23 +44,23 @@ class WaterCloudApplicationTests {
 		System.out.println(ret.toString());
 	}
 
-//	@Test
-//	public void test_Daily_charge_and_Monthly_charge(){
-//		String str = "2022-06-07";
-//		//现将年月日切分出来
-//		String[] arr = str.split("-");
-//		int year = Integer.parseInt(arr[0]);
-//		int month = Integer.parseInt(arr[1]);
-//		int day = Integer.parseInt(arr[2]);
-//		List<Daily_charge> list = projectMapper.daily_charge(year,month,day);
-//		for (Daily_charge i: list){
-//			System.out.println(i.toString());
-//		}
-//		List<Monthly_charge> list2 = projectMapper.monthly_charge(year,month);
-//		for (Monthly_charge i: list2){
-//			System.out.println(i.toString());
-//		}
-//	}
+	@Test
+	public void test_Daily_charge_and_Monthly_charge(){
+		String str = "2022-06-07";
+		//现将年月日切分出来
+		String[] arr = str.split("-");
+		int year = Integer.parseInt(arr[0]);
+		int month = Integer.parseInt(arr[1]);
+		int day = Integer.parseInt(arr[2]);
+		List<Daily_charge> list = projectMapper.daily_charge(year,month,day);
+		for (Daily_charge i: list){
+			System.out.println(i.toString());
+		}
+		List<Monthly_charge> list2 = projectMapper.monthly_charge(year,month);
+		for (Monthly_charge i: list2){
+			System.out.println(i.toString());
+		}
+	}
 	@Test
 	public void test_project(){
 		List<ChargeDay> chargeDays = projectMapper.selectByUserAndUseWaterAndProject();
